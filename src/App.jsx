862d4ceaf70c6ccb53fde9f7e123ec3242b91b7a
hjp <2523863783@qq.com>
2021-08-28 21:46:42 +0800

    config for antd-mobile

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Button,WhiteSpace } from 'antd-mobile';
+
+export default function App() {
+  return (
+    <div>
+      hello world
+      <Button type="primary">primary</Button><WhiteSpace />
+    </div>
+  )
+}
