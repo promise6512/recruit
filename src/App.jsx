9fd286c7bc14b1ced60eb3dd26ae793a07da4ad3
hjp <2523863783@qq.com>
2021-09-03 13:30:55 +0800

    删除多余的app.jsx文件

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { Button,WhiteSpace } from 'antd-mobile';
-export default function App() {
-  return (
-    <div>
-      hello world
-      <Button type="primary">primary</Button><WhiteSpace />
-    </div>
-  )
-}
